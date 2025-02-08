5662bdd783a431289997f265a28b3a411bdc24bd
Fma <info@fmakdemir.com>
2025-02-08 00:06:41 +0000

    feat: added new api for solving wordle inputs

---
+++ fwordlesolver_api/api.py
@@ -0,0 +1,27 @@
+from ninja import NinjaAPI
+from fwordlesolver.solver import WordleSolver
+
+api = NinjaAPI()
+
+
+@api.get("/hello")
+def hello(request):
+    return "Hello world"
+
+
+@api.post("/wordle")
+def solve_world(request, words: list[str], places: list[str]):
+    if not len(words):
+        return {"error": "Must provide at least one word"}
+    if len(places) != len(words):
+        return {"error": "Number of words and places must match"}
+    if any(len(w) != len(p) for w, p in zip(words, places)):
+        return {"error": "Words and places must have the same length"}
+    word_size = len(words[0])
+    solver = WordleSolver(word_size)
+    for _w, _p in zip(words, places):
+        solver.apply_guess(_w, _p)
+    return {
+        "sug": solver.get_suggestions(),
+        "alt": solver.get_not_used_suggestion(),
+    }
