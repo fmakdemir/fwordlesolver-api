eff272b0856d9c4d7c9c85808bd96ff023f8c193
fmakdemir <1462379+fmakdemir@users.noreply.github.com>
2025-02-10 00:49:51 +0000

    feat: fixes for generated schema

--- fwordlesolver_api/api.py
+++ fwordlesolver_api/api.py
@@ -1,24 +1,34 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 from fwordlesolver.solver import WordleSolver
 
-api = NinjaAPI()
+api = NinjaAPI(title="Wordle API", description="This is an API for Wordle")
 
 
-@api.post("/wordle")
-def solve_world(request, words: list[str], places: list[str]):
-    if not len(words):
-        return {"error": "Must provide at least one word"}
-    if len(places) != len(words):
+class WordleSolveRequest(Schema):
+    words: list[str]
+    places: list[str]
+    size: int
+
+
+class WordleSolveResponse(Schema):
+    suggestions: list[str]
+    alternatives: list[str]
+    remaining: int
+    used_letters: set[str]
+
+
+@api.post("/wordle", response=WordleSolveResponse)
+def solve_wordle(request, req: WordleSolveRequest) -> WordleSolveResponse:
+    if len(req.places) != len(req.words):
         return {"error": "Number of words and places must match"}
-    if any(len(w) != len(p) for w, p in zip(words, places)):
+    if any(len(w) != len(p) for w, p in zip(req.words, req.places)):
         return {"error": "Words and places must have the same length"}
-    word_size = len(words[0])
-    solver = WordleSolver(word_size)
-    for _w, _p in zip(words, places):
+    solver = WordleSolver(req.size)
+    for _w, _p in zip(req.words, req.places):
         solver.apply_guess(_w, _p)
-    return {
-        "sug": solver.get_suggestions(),
-        "alt": solver.get_not_used_suggestion(),
-        "remaining": len(solver.words),
-        "used_letters": solver.used_letters,
-    }
+    return WordleSolveResponse(
+        solver.get_suggestions(),
+        solver.get_not_used_suggestion(),
+        len(solver.words),
+        solver.used_letters,
+    )
